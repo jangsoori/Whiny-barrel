c5244f3da99cd6ec4be738cc83ff66e20b0bc0b4
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-29 16:15:17 +0000

    Added Theme to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import GlobalStyles from "../styles/GlobalStyles";
-
+import { ThemeProvider } from "emotion-theming";
+import { theme } from "../styles/theme";
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
