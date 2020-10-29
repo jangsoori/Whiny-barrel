42d71dcfbe072168ec495465da529e5d0f8e7d91
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-29 10:17:48 +0000

    Reset all things

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,20 @@
+import { Global, css } from "@emotion/core";
+import emotionReset from "emotion-reset";
+import React from "react";
+
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={css`
+        ${emotionReset}
+
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+      `}
+    />
+  );
+}
