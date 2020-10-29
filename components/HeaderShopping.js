4a0328807abf83d7f859bbbc4b6e0df218279ba1
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-29 16:14:19 +0000

    Added headers shopping cart

---
+++ components/HeaderShopping.js
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import React from "react";
+const StyledHeaderShopping = styled.section`
+  color: white;
+  display: grid;
+  grid-auto-flow: column;
+  column-gap: 2rem;
+  align-items: center;
+  align-content: center;
+  height: 100%;
+  p {
+    font-size: 2rem;
+  }
+`;
+export default function HeaderShopping() {
+  return (
+    <StyledHeaderShopping>
+      <i className="far fa-heart fa-2x"></i>
+      <i className="fas fa-shopping-bag fa-2x"></i>
+      <p>0.00 GBP</p>
+    </StyledHeaderShopping>
+  );
+}
