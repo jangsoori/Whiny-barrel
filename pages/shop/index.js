98c1cc1aa8dc179e991087fb22159061c5b257c3
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-30 14:18:41 +0000

    Added shop home page

---
+++ pages/shop/index.js
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+import React from "react";
+import ShopLayout from "../../components/ShopLayout";
+import ShopMain from "../../components/ShopMain";
+import { server } from "../../config/index";
+const StyledShop = styled.section``;
+export default function index({ categories, items }) {
+  return (
+    <StyledShop>
+      <ShopLayout>
+        <ShopMain categories={categories} items={items} />
+      </ShopLayout>
+    </StyledShop>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${server}/items.json`);
+  const data = await res.json();
+  const categories = [];
+  //get categories
+  data.forEach((item) => {
+    categories.push(item.country);
+  });
+
+  return {
+    props: {
+      items: data,
+      //remove duplicates with Set
+      categories: [...new Set(categories)],
+    },
+  };
+}
