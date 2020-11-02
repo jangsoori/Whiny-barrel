49342d97230a681bd276891f0ff618ebf7184cbf
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-02 12:20:15 +0000

    Added action to item in cart

--- components/cart/Item.js
+++ components/cart/Item.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import React from "react";
 import Image from "next/image";
+import useCartActions from "../../hooks/useCartActions";
 const StyledItem = styled.li`
   display: grid;
   grid-template-columns: 1fr 2fr 1fr 1fr;
@@ -10,7 +11,7 @@ const StyledItem = styled.li`
 const Info = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   height: 100%;
 `;
 const Name = styled.p`
@@ -23,20 +24,26 @@ const Price = styled.p`
 `;
 const Qty = styled.p``;
 const Actions = styled.section``;
+const Action = styled.button`
+  border: none;
+  background: ${({ theme }) => theme.colors.primary};
+  color: white;
+  padding: 1rem 1.5rem;
+  border-radius: 0.5rem;
+`;
+
 export default function Item({ item }) {
+  const { deleteFromCart } = useCartActions();
   return (
     <StyledItem>
       <Image src={`/pics/${item.picture}`} width={50} height={70} />
       <Info>
         <Name>{item.name}</Name>
-        <Price>
-          <span>Price: </span> {item.price.toFixed(2)} GBP
-        </Price>
+        <Price>{item.price.toFixed(2)} GBP</Price>
       </Info>
-      <Qty>Qty: X</Qty>
+      <Qty>Qty: {item.qty}</Qty>
       <Actions>
-        <p>add</p>
-        <p>remove</p>
+        <Action onClick={() => deleteFromCart(item._id)}>Remove</Action>
       </Actions>
     </StyledItem>
   );
