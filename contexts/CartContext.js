3f399dbbea45eb09e6ba2ea632ac96bd96f83525
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-02 12:20:51 +0000

    Removed duplicated items in cart and counted.

--- contexts/CartContext.js
+++ contexts/CartContext.js
@@ -1,15 +1,28 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    //Make copy of array, find index of item in array. If exists, add quanity, if not, add item with quantity of 1
+    const newarr = cart;
+    const index = newarr.findIndex((x) => x._id === item._id);
+    if (index > -1) {
+      newarr[index].qty++;
+      setCart([...newarr]);
+    } else {
+      item.qty = 1;
+      setCart([...cart, item]);
+    }
   };
 
   const deleteFromCart = (id) => {
-    setCart(cart.filter((el) => el.id !== id));
+    setCart(cart.filter((el) => el._id !== id));
   };
+
   return (
     <CartContext.Provider
       value={{ cart, actions: { addToCart, deleteFromCart } }}
