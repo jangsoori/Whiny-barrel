26b416f43ddf06a4afadd6b760fc2dae3ab6f6b5
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-11-02 12:27:28 +0000

    Added total cart items count

--- hooks/useCart.js
+++ hooks/useCart.js
@@ -3,14 +3,17 @@ import { CartContext } from "../contexts/CartContext";
 
 export default function useCart() {
   const [value, setValue] = React.useState(0);
+  const [count, setCount] = React.useState(0);
   const { cart } = useContext(CartContext);
   useEffect(() => {
     const cartValue = cart.reduce(
       (prev, current) => prev + current.price * current.qty,
       0
     );
+    const cartCount = cart.reduce((prev, current) => prev + current.qty, 0);
     setValue(cartValue);
+    setCount(cartCount);
   }, [cart]);
 
-  return { value, cart };
+  return { value, count, cart };
 }
